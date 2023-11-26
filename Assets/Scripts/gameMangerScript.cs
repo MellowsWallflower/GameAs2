d0246acf536887cd2b4a7e47b346c07cde325bc4
hamsterisnotcute <144809073+hamsterisnotcute@users.noreply.github.com>
2023-11-26 15:06:51 -0500

    restart button

--- Assets/Scripts/gameMangerScript.cs
+++ Assets/Scripts/gameMangerScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gameMangerScript : MonoBehaviour
 {
@@ -20,4 +21,10 @@ public class gameMangerScript : MonoBehaviour
     {
         gameOverUI.SetActive(true);
     }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
