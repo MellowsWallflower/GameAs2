d3aa51afe25a098f7a12f29b56e1880030e473bf
hamsterisnotcute <144809073+hamsterisnotcute@users.noreply.github.com>
2023-11-26 15:14:04 -0500

    gameover screen up when time is 0

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -7,6 +7,8 @@ public class TimerController : MonoBehaviour
 {
    public float initialTime = 120f;
     private float currentTime;
+    public gameMangerScript gameManager;
+    private bool isReached;
 
     public TextMeshProUGUI timerText;
 
@@ -23,8 +25,10 @@ public class TimerController : MonoBehaviour
             currentTime -= Time.deltaTime;
             UpdateTimerDisplay();
         }
-        else
+        else if(currentTime <= 0)
         {
+            isReached = true;
+            gameManager.gameOver();
             Debug.Log("Time's up!");
         }
     }
