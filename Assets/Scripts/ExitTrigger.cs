173c253994677cf8b3e05eee77a64cc5fc5ac403
hamsterisnotcute <144809073+hamsterisnotcute@users.noreply.github.com>
2023-11-26 14:56:53 -0500

    Gameover Screen

--- Assets/Scripts/ExitTrigger.cs
+++ Assets/Scripts/ExitTrigger.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class ExitTrigger : MonoBehaviour
 {
+    public gameMangerScript gameManager;
+    private bool isReached;
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.name == "Player" && !isReached)
         {
             Debug.Log("Player reached the exit!");
+            isReached = true;
+            gameManager.gameOver();
+
         }
     }
 }
